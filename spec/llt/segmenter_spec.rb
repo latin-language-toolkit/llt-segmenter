f12f0e12d42ff20eb091179ac9f31edc984689a4
Christof Sirk <christof.sirk@gmail.com>
2013-11-22 20:18:45 +0100

    Add spec task as default task and fix specs.

--- spec/llt/segmenter_spec.rb
+++ spec/llt/segmenter_spec.rb
@@ -4,8 +4,4 @@ describe Llt::Segmenter do
   it 'should have a version number' do
     Llt::Segmenter::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
 end
